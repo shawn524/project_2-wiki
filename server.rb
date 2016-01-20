aa40ca347b6f1f5a129e2a10587549a06e3e781e
Shawn <shawn524@gmail.com>
2016-01-20 18:29:19 -0500

    Login maybe working

--- server.rb
+++ server.rb
@@ -25,7 +25,7 @@ module Wiki
       password = BCrypt::Password.create(params["password"])
       email = params["email"]
 
-      conn.exec_params("INSERT INTO users (user_name, user_password, user_email) VALUES ($1,$2,$3);",[user_name, passwd, email])
+      conn.exec_params("INSERT INTO users (user_name, user_password, user_email) VALUES ($1,$2,$3);",[user_name, password, email])
       redirect "/"
     end
 
@@ -33,8 +33,26 @@ module Wiki
       erb :login
     end
 
-    post "/login" do
+    post '/login' do
+      user_name = params['user_name']
+      password = params['password']
 
+      returning_user = conn.exec_params("SELECT * FROM users WHERE user_name = $1;",[user_name]).first
+      # binding.pry
+      if returning_user.any?
+        check = BCrypt::Password.new(returning_user['user_password'])
+        if check == password
+          # this user_id is just a var, but now they are tagged?
+          session['user_id'] = returning_user['user_id']
+          session['logged_in'] = true
+          # binding.pry
+          erb :index
+        else
+          erb :index
+        end
+      else
+        redirect '/signup'
+      end
     end
 
     get "/new_article" do
